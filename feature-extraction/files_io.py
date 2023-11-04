ef598ed31fec8f6ef683733408f3857f5834ca38
AlpacaMoon <thongxsgaming@gmail.com>
2023-11-04 17:30:52 +0800

    file arrangements

--- feature-extraction/files_io.py
+++ feature-extraction/files_io.py
@@ -8,7 +8,7 @@ def readActionLabels():
     action_labels = []
     with open(ACTION_LABELS_PATH) as f:
         csv_reader = csv.reader(f, delimiter=",")
-        action_labels = [each[1] for each in csv_reader]
+        action_labels = {each[0]: each[1] for each in csv_reader}
     return action_labels
 
 def readActionMapping():
@@ -19,17 +19,20 @@ def readActionMapping():
         
     return action_mapping
 
+def secureFilename(filename, replacement_char="_"):
+    invalid_chars_pattern = r'[\/:*?"<>|]'
+    return re.sub(invalid_chars_pattern, replacement_char, filename)
 
 def initActionLabelFolders(action_labels):
     # Create folder/path for each action label
-    for action in action_labels:
-        if not os.path.exists(os.path.join(KEYPOINTS_PATH, action)):
-            os.makedirs(os.path.join(KEYPOINTS_PATH, action))
+    for i, action in action_labels.items():
+        folder_name = f"{i},{secureFilename(action)}"
+        if not os.path.exists(os.path.join(KEYPOINTS_PATH, folder_name)):
+            os.makedirs(os.path.join(KEYPOINTS_PATH, folder_name))
 
 
-def saveKeypoints(action, filename, keypoints, replacement_char="_"):
-    invalid_chars_pattern = r'[\/:*?"<>|]'
-    _action = re.sub(invalid_chars_pattern, replacement_char, action)
-    _filename = re.sub(invalid_chars_pattern, replacement_char, filename)
+def saveKeypoints(action, filename, keypoints):
+    _action = secureFilename(action)
+    _filename = secureFilename(filename)
     
     np.save(os.path.join(KEYPOINTS_PATH, _action, _filename), keypoints)
