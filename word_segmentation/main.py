39ecb3de8bcbdcfd662d45949d54729633aca508
EngLip2002 <englip455@gmail.com>
2023-12-26 00:16:29 +0800

    l

--- word_segmentation/main.py
+++ word_segmentation/main.py
@@ -3,8 +3,6 @@ import wordninja as segmenter
 class WordSegmentationModule:
     def split(self, characters):
         self.word_array = segmenter.split(characters)
-        temp = []
-        # return ", ".join(self.word_array).lower()
         for i in range(len(self.word_array)):
             self.word_array[i] = self.word_array[i].lower()
         return self.word_array
